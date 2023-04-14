7cc58fc09087231add19cda566f3a85e3fb78f16
Mazhar31 <mzrbutt31@gmail.com>
2023-04-14 20:25:16 +0500

    Outputs in Java

---
+++ 2. Input-Output and Variables + Data types/One_output.java
@@ -0,0 +1,8 @@
+public class One_output {
+    public static void main(String[] args) {
+        // Output
+        System.out.println("Hello World!");
+        System.out.print("Output without line break!");
+        System.out.print("Hello World\nwith Java");
+    }
+}
---
+++ 2. Input-Output and Variables + Data types/Two_output_exercise.java
@@ -0,0 +1,16 @@
+public class Two_output_exercise {
+    public static void main(String[] args) {
+        /*
+         * Qs. Print the following pattern:
+         * *
+         * **
+         * ***
+         * ****
+         */
+        // Solution
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
