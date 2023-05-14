4901babe5adbc50a53437b41d7489eee81ef0821
Mazhar31 <mzrbutt31@gmail.com>
2023-05-14 22:26:10 +0500

    insertion sorting in java

---
+++ p_3_inserionSort.java
@@ -0,0 +1,32 @@
+public class p_3_inserionSort {
+    // function to print array
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 6, 3, 7, 8, 1 };
+
+        // insertion sort
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            while (j >= 0) {
+                if (arr[j] > temp) {
+                    arr[j + 1] = arr[j];
+                    j--;
+                } else {
+                    break;
+                }
+
+            }
+            arr[j + 1] = temp;
+
+        }
+
+        printArray(arr);
+    }
+}
