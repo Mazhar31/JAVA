9c7c92060c52582eec2c90c8b13993d078230542
Mazhar31 <mzrbutt31@gmail.com>
2023-05-14 22:31:52 +0500

    insertion sort

--- 11. Sorting in Java/p_3_inserionSort.java
+++ 11. Sorting in Java/p_3_inserionSort.java
@@ -14,13 +14,9 @@ public class p_3_inserionSort {
         for (int i = 1; i < arr.length; i++) {
             int temp = arr[i];
             int j = i - 1;
-            while (j >= 0) {
-                if (arr[j] > temp) {
-                    arr[j + 1] = arr[j];
-                    j--;
-                } else {
-                    break;
-                }
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j--;
 
             }
             arr[j + 1] = temp;
